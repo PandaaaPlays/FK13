f10cbbd5d4a4f65c288b7e0a86d1255a5b3c1729
etienne.toupin <etienne.toupin@usherbrooke.ca>
2025-06-23 21:04:20 -0400

    Initial commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Angular-FK13',
-  webDir: 'dist/angular-fk13'
+  webDir: 'dist/angular-fk13/browser'
 };
 
 export default config;

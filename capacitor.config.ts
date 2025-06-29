1b2a539b496bcdbc98fde43b71333617f5a9aa7b
etienne.toupin <etienne.toupin@usherbrooke.ca>
2025-06-29 16:58:37 -0400

    Initial testing

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Angular-FK13',
-  webDir: 'dist/angular-fk13/browser'
-};
-
-export default config;

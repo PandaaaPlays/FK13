0e1d8a34dd99a51c51a839d5f9e378576f531ff6
etienne.toupin <etienne.toupin@usherbrooke.ca>
2025-07-01 16:48:03 -0400

    Downloadable app test 1

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import {provideHttpClient} from '@angular/common/http';
+import {provideServiceWorker} from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
+    provideServiceWorker('ngsw-worker.js'),
   ]
 }).catch((err) => console.error(err));
